a8b44ae510af180595bce3f0fefea23ba848f770
Eugene Khristo <marteau9292@gmail.com>
2025-04-20 22:21:05 +0300

    Abstract classes and Interfaces

--- src/index.ts
+++ src/index.ts
@@ -79,3 +79,33 @@ class Teacher extends Person {
     return `Professor ${super.fullName}`;
   }
 }
+
+abstract class Shape {
+  constructor(public color: string) {}
+
+  abstract render(): void;
+}
+
+interface Calendar {
+  name: string;
+  addEvent(): void;
+  removeEvent(): void;
+}
+
+interface CloudCalendar extends Calendar {
+  sync(): void;
+}
+
+class GoogleCalendar implements CloudCalendar {
+  constructor(public name: string) {}
+
+  sync(): void {
+    throw new Error('Method not implemented.');
+  }
+  addEvent(): void {
+    throw new Error('Method not implemented.');
+  }
+  removeEvent(): void {
+    throw new Error('Method not implemented.');
+  }
+}
